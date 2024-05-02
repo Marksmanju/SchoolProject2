62b0e945c0e5291f7edef00988463b785413009d
Marksmanju <163834304+Marksmanju@users.noreply.github.com>
2024-05-02 09:35:00 +0200

    Add files via upload

---
+++ SchoolProjectRight/src/main/java/org/example/objects/Tile.java
@@ -0,0 +1,25 @@
+package org.example.objects;
+
+public class Tile {
+    private int x;
+    private int y;
+    private String type;
+
+    public Tile(int x, int y, String type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
